9ba747cda67ba721288b4e2d27fe56f2d5f4ed3a
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-11-16 19:10:49 -0700

    updated schema and some back end functions

--- server/database.js
+++ server/database.js
@@ -4,32 +4,78 @@ const sql = require('mssql');
 const config = {
   user: 'pmacd',
   password: 'asdfghjkl3388',
-  server: 'WORKSTATION', // You can use 'localhost' if SQL Server is on your local machine
+  server: 'WORKSTATION',
   database: 'quizzes',
   options: {
-     encrypt: true, // Use this if you're on Windows Azure
+    encrypt: true,
     trustServerCertificate: true,
   },
 };
 
-// Connect to the database
-sql.connect(config)
-  .then((pool) => {
+// Create a pool object
+const pool = new sql.ConnectionPool(config);
+
+// Function to connect to the database
+async function connectToDatabase() {
+  try {
+    await pool.connect();
     console.log('Connected to the database');
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
+  }
+}
 
-    // Execute a simple query
-    //return pool.request().query('SELECT 1 as Result');
+// Function to get tests
+async function getTests() {
+  try {
+    const result = await pool.request().query('SELECT * FROM quizzes.dbo.tests');
+    console.dir(result.recordset);
+    return result.recordset;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    return pool.request().query('SELECT * FROM quizzes.dbo.Answers');
-  })
-  .then((result) => {
-    console.dir(result);
-    //console.log('Query Result:', result.recordset[0].Result);
-  })
-  .catch((err) => {
-    console.error('Error:', err);
-  })
-  .finally(() => {
-    // Close the connection pool
-    sql.close();
-  });
+// Function to get questions
+async function getQuestions() {
+  try {
+    const result = await pool.request().query('SELECT * FROM quizzes.dbo.questions');
+    console.dir(result.recordset);
+    return result.recordset;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getQuestionsByTestId(test_id) {
+  try {
+    const result = await pool.request().query(`SELECT * FROM quizzes.dbo.questions WHERE test_id = ${test_id}`);
+    console.dir(result.recordset);
+    return result.recordset;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAnswersByQuestionId(question_id) {
+  try {
+    const result = await pool.request().query(`SELECT * FROM quizzes.dbo.answers WHERE question_id = ${question_id}`);
+    console.dir(result.recordset);
+    return result.recordset;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+// Connect to the database and then call the functions
+async function initialize() {
+  await connectToDatabase();
+  //await getTests();
+  //await getQuestions();
+  //await getQuestionsByTestId(1);
+  await getAnswersByQuestionId(2);
+}
+
+// Call the initialization function
+initialize();
--- server/schema.sql
+++ server/schema.sql
@@ -42,33 +42,92 @@ INSERT INTO questions (question_id, test_id, question_text) VALUES
     (15, 3, 'What is the shape of a pentagon?');
 
 INSERT INTO answers (answer_id, question_id, answer_text, correct) VALUES
-    (1, 1, '1', 1),
-    (2, 1, '2', 0),
+    -- Answers for Question 1
+    (1, 1, '1', 0),
+    (2, 1, '2', 1),
     (3, 1, '3', 0),
     (4, 1, '4', 0),
-    (5, 1, '5', 0),
-    (6, 2, '3', 0),
-    (7, 2, 'Mount Everest', 1),
-    (8, 2, 'Pacific Ocean', 0),
-    (9, 2, 'Vatican City', 0),
-    (10, 2, 'Russia', 0),
-    (11, 3, 'Square', 0),
-    (12, 3, 'Circle', 1),
-    (13, 3, 'Triangle', 0),
-    (14, 3, 'Rectangle', 0),
-    (15, 3, 'Pentagon', 0),
-    (16, 1, '6', 0),
-    (17, 1, '7', 0),
-    (18, 1, '8', 0),
-    (19, 1, '9', 0),
-    (20, 1, '10', 0),
-    (21, 2, 'Mount Kilimanjaro', 0),
-    (22, 2, 'Mount Denali', 0),
-    (23, 2, 'Mount Kilimanjaro', 0),
-    (24, 2, 'Mount Everest', 0),
-    (25, 2, 'Mount Fuji', 0),
-    (26, 3, 'Hexagon', 0),
-    (27, 3, 'Octagon', 0),
-    (28, 3, 'Heptagon', 0),
-    (29, 3, 'Rhombus', 0),
-    (30, 3, 'Trapezoid', 0);
+    
+    -- Answers for Question 2
+    (5, 2, '3', 0),
+    (6, 2, '4', 0),
+    (7, 2, '5', 0),
+    (8, 2, '6', 1),
+    
+    -- Answers for Question 3
+    (9, 3, '4', 0),
+    (10, 3, '6', 0),
+    (11, 3, '9', 1),
+    (12, 3, '12', 0),
+    
+    -- Answers for Question 4
+    (13, 4, '7', 0),
+    (14, 4, '8', 1),
+    (15, 4, '10', 0),
+    (16, 4, '14', 0),
+    
+    -- Answers for Question 5
+    (17, 5, '9', 0),
+    (18, 5, '10', 0),
+    (19, 5, '11', 1),
+    (20, 5, '15', 0),
+    
+    -- Answers for Question 6
+    (21, 6, 'Ottawa', 1),
+    (22, 6, 'Toronto', 0),
+    (23, 6, 'Vancouver', 0),
+    (24, 6, 'Montreal', 0),
+    
+    -- Answers for Question 7
+    (25, 7, 'Mount Everest', 1),
+    (26, 7, 'K2', 0),
+    (27, 7, 'Kangchenjunga', 0),
+    (28, 7, 'Lhotse', 0),
+    
+    -- Answers for Question 8
+    (29, 8, 'Atlantic Ocean', 0),
+    (30, 8, 'Indian Ocean', 0),
+    (31, 8, 'Southern Ocean', 0),
+    (32, 8, 'Pacific Ocean', 1),
+    
+    -- Answers for Question 9
+    (33, 9, 'Vatican City', 1),
+    (34, 9, 'Monaco', 0),
+    (35, 9, 'Nauru', 0),
+    (36, 9, 'Tuvalu', 0),
+    
+    -- Answers for Question 10
+    (37, 10, 'Russia', 1),
+    (38, 10, 'Canada', 0),
+    (39, 10, 'United States', 0),
+    (40, 10, 'China', 0),
+    
+    -- Answers for Question 11
+    (41, 11, 'Circle', 1),
+    (42, 11, 'Square', 0),
+    (43, 11, 'Triangle', 0),
+    (44, 11, 'Rectangle', 0),
+    
+    -- Answers for Question 12
+    (45, 12, 'Circle', 0),
+    (46, 12, 'Square', 1),
+    (47, 12, 'Triangle', 0),
+    (48, 12, 'Rectangle', 0),
+    
+    -- Answers for Question 13
+    (49, 13, 'Circle', 0),
+    (50, 13, 'Square', 0),
+    (51, 13, 'Triangle', 1),
+    (52, 13, 'Rectangle', 0),
+    
+    -- Answers for Question 14
+    (53, 14, 'Circle', 0),
+    (54, 14, 'Square', 0),
+    (55, 14, 'Triangle', 0),
+    (56, 14, 'Rectangle', 1),
+    
+    -- Answers for Question 15
+    (57, 15, 'Circle', 0),
+    (58, 15, 'Square', 0),
+    (59, 15, 'Triangle', 0),
+    (60, 15, 'Pentagon', 1);
