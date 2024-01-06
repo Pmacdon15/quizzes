9bd78fad521674ad35587cf6bf79bf48d9567b69
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-01-06 16:55:51 -0700

    Add user results fetching functionality

--- client/src/app/results/[user_email]/page.jsx
+++ client/src/app/results/[user_email]/page.jsx
@@ -1,7 +1,27 @@
-
+import React, { useState } from "react";
+import Link from "next/link";
 import "../../page.css"
 
 const Results = ({ params }) => {
+    const user_email = decodeURIComponent(params.user_email);
+    console.log(user_email);
+
+    // fetch user results from the database
+    const [userResults, setUserResults] = useState([]);
+
+    const fetchUserResults = async () => {
+        const response = await fetch(
+            `http://localhost:5544/user/${user_email}`
+        ).then((res) => res.json());
+        console.log("response", response);
+
+        setUserResults(response);        
+    };
+    fetchUserResults();
+
+
+    
+
     return (
         <div className="Container">
             <div className="header">
