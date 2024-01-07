f3785b2b25d3d2859fb9f6e5db2c359ea8aeab62
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-01-06 17:25:06 -0700

    Add MUI Table and fetch user results from the database

--- client/src/app/results/[user_email]/page.jsx
+++ client/src/app/results/[user_email]/page.jsx
@@ -1,36 +1,75 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import "../../page.css"
+import "../../page.css";
+
+// import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Results = ({ params }) => {
-    const user_email = decodeURIComponent(params.user_email);
-    console.log(user_email);
+  const user_email = decodeURIComponent(params.user_email);
+  console.log(user_email);
 
-    // fetch user results from the database
-    const [userResults, setUserResults] = useState([]);
+  // fetch user results from the database
+  const [userResults, setUserResults] = useState([]);
 
+  useEffect(() => {
     const fetchUserResults = async () => {
-        const response = await fetch(
-            `http://localhost:5544/user/${user_email}`
-        ).then((res) => res.json());
-        console.log("response", response);
-
-        setUserResults(response);        
+      const response = await fetch(
+        `http://localhost:5544/results/${user_email}`
+      ).then((res) => res.json());
+      console.log("response", response);
+      // Remove first result from array
+      
+      setUserResults(response);
     };
     fetchUserResults();
+  }, [user_email]);
 
-
-    
-
-    return (
-        <div className="Container">
-            <div className="header">
-                <div className="text">Results</div>
-                <div className="underline"></div>
-            </div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="Container">
+      <div className="header">
+        <div className="text">Results</div>
+        <div className="underline"></div>
+      </div>
+        {/* loop threw userResults and display them  in a table */}
+        <TableContainer component={Paper} >
+          <Table sx={{ maxWidth: "250px" }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Quiz</TableCell>
+                <TableCell align="center">Correct</TableCell>
+                <TableCell align="center">Incorrect</TableCell>
+                <TableCell align="center">Amount of Questions</TableCell>
+                <TableCell align="center">Date Taken</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {userResults.map((result) => (
+                <TableRow
+                  key={result.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0} }}
+                >
+                  <TableCell component="th" scope="row">
+                    {result.test_name}
+                  </TableCell>
+                  <TableCell align="center">{result.total_correct}</TableCell>
+                  <TableCell align="center">{result.total_questions - result.total_correct }</TableCell>
+                  <TableCell align="center">{result.total_questions}</TableCell>
+                  <TableCell align="center">{result.date_taken}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    </div>
+  );
+};
 
 export default Results;
