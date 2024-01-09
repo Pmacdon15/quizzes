095ef28f6d5fc61a20280a72190886e281850da9
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-01-09 14:08:59 -0700

    Delete Quiz component and update page.css

--- client/src/app/editQuizzes/[user_email]/page.jsx
+++ client/src/app/editQuizzes/[user_email]/page.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 
-import "../page.css";
+import "../../page.css";
 
 const Quiz = ({ params }) => {
-  const quizSlugs = ["Math", "Places", "Shapes"];
+  // const quizSlugs = ["Math", "Places", "Shapes"];
   const userEmail = decodeURIComponent(params.user_email);
 
   const [user, setUser] = useState([]);
@@ -28,6 +28,33 @@ const Quiz = ({ params }) => {
     fetchUserResults();
   }, [userEmail]);
 
+  const [quizSlugs, setQuizSlugs] = useState([]);
+
+  const fetchQuizSlugs = async () => {
+    const response = await fetch(`http://localhost:5544/tests`).then((res) =>
+      res.json()
+    );
+    console.log("response", response);
+    // Loop through the response and add response[i].test_name as a slug
+    const slugs = [];
+    for (let i = 0; i < response.length; i++) {
+      slugs.push(response[i].test_name);
+    }
+    setQuizSlugs(slugs);
+  };
+
+  useEffect(() => {
+    fetchQuizSlugs();
+  }, []);
+
+  const deleteQuiz = async (slug) => {
+    const response = await fetch(`http://localhost:5544/test/${slug}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+    console.log("response", response);
+    fetchQuizSlugs();
+  };
+
   return (
     <div className="container">
       <div className="header">
@@ -36,27 +63,30 @@ const Quiz = ({ params }) => {
       </div>
       <div className="submit-container">
         {quizSlugs.map((slug) => (
-          <div className="test">
+          <div className="test" key={slug}>
             <label>{slug}</label>
             <Button
               variant="contained"
               color="primary"
               style={{ margin: "5px" }}
             >
-              delete
+              Edit
             </Button>
             <Button
               variant="contained"
               color="primary"
               style={{ margin: "5px" }}
+              onClick={() => deleteQuiz(slug)}
             >
-              edit
+              Delete
             </Button>
           </div>
         ))}
         <TextField />
-            <Button variant="contained" color="primary" style={{ margin: "5px" }}>Add Quiz</Button>
-              </div>
+        <Button variant="contained" color="primary" style={{ margin: "5px" }}>
+          Add Quiz
+        </Button>
+      </div>
       <div className="options-container">
         <Link
           href={user.admin ? `/menuAdmin/${userEmail}` : `/menu/${userEmail}`}
